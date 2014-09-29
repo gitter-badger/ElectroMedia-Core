6cc22ce1165af854a5d9b11e1cc0eb09dea0a023
etkirsch <etkirsch@gmail.com>
2014-09-28 22:49:33 -0400

    More tests

--- DubstepCannon/FFTPreprocessing.h
+++ DubstepCannon/FFTPreprocessing.h
@@ -13,5 +13,6 @@ double* prepareAndExecuteFFT(const int* dataIn);
 // Fast Fourier Transform
 fftw_complex* fastFourierTransform(const double* data);
 
+// More Tests
 long calculateDataFileSize(char* fileName);
 int* obtainDataFromFile(char* fileName, long fileSize);
